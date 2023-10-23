0e3707b76cf0df60e3d669183cf5976013694419
jackmaple2 <jackmaple3000@gmail.com>
2023-10-23 19:21:56 +0100

    dynamically extracts information from multiple pages

--- env/web-scraper.py
+++ env/web-scraper.py
@@ -1,17 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
-target_page = requests.get('https://www.infobel.com/en/uk/business/10000/food_restaurants')
+def web_scraper(url):
+    target_page = requests.get(url)
+    soup = BeautifulSoup(target_page.text, 'html.parser')
 
-soup = BeautifulSoup(target_page.text, 'html.parser')
-
-restaurants = soup.findAll('h2', attrs={'class': 'customer-item-name'})
-addresses = soup.findAll('span', attrs={'class': 'customer-info-detail highlighted address'})
+    restaurants = soup.findAll('h2', attrs={'class': 'customer-item-name'})
+    addresses = soup.findAll('span', attrs={'class': 'customer-info-detail highlighted address'})
 
 
-for restaurant, address in zip(restaurants, addresses):
-    print('Restaurant: ' + restaurant.text)
-    print('Address: ' + address.text)
+    for restaurant, address in zip(restaurants, addresses):
+        print('Restaurant: ' + restaurant.text)
+        print('Address: ' + address.text)
+
+base_url = 'https://www.infobel.com/en/uk/business/10000/food_restaurants'
+page_number = 1
+
+while True:
+    page_url = f'{base_url}/{page_number}'
+    web_scraper(page_url)
+
+    page_number += 1
+
+    if page_number > 3:
+        break
+
+
     
 
 
