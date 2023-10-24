4d762b31039a2735c8c414ec64ab91a5962689cf
jackmaple2 <jackmaple3000@gmail.com>
2023-10-24 13:57:58 +0100

    returns info in csv file

--- env/web-scraper.py
+++ env/web-scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 def web_scraper(url):
     target_page = requests.get(url)
@@ -8,10 +9,16 @@ def web_scraper(url):
     restaurants = soup.findAll('h2', attrs={'class': 'customer-item-name'})
     addresses = soup.findAll('span', attrs={'class': 'customer-info-detail highlighted address'})
 
+    restaurant_directory = open('scraped_restaurants.csv', 'w', newline='')
+    writer = csv.writer(restaurant_directory)
+
+    writer.writerow(['restaurants', 'addresses'])
 
     for restaurant, address in zip(restaurants, addresses):
         print('Restaurant: ' + restaurant.text)
         print('Address: ' + address.text)
+        writer.writerow([restaurant.text, address.text])
+    restaurant_directory.close()
 
 base_url = 'https://www.infobel.com/en/uk/business/10000/food_restaurants'
 page_number = 1
